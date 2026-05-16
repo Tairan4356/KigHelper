92e78206ec277e5e912743b5194d1156efd2dbcb
Ziegler <liang_tr@hotmail.com>
2026-05-16 17:39:25 +0800

    [FEAT] Update version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ziegler.kighelper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2-preview"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
