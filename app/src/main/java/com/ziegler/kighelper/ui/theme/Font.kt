9758cf6ef730909b8a822a7182a0751ce7911f26
Ziegler <liang_tr@hotmail.com>
2026-06-21 19:20:20 +0800

    [REF] shrink font files

--- app/src/main/java/com/ziegler/kighelper/ui/theme/Font.kt
+++ app/src/main/java/com/ziegler/kighelper/ui/theme/Font.kt
@@ -13,12 +13,7 @@ object CustomFonts {
     )
 
     val MiSans = FontFamily(
-        Font(R.font.misans_thin, FontWeight.Thin),
-        Font(R.font.misans_extralight, FontWeight.ExtraLight),
-        Font(R.font.misans_light, FontWeight.Light),
         Font(R.font.misans_normal, FontWeight.Normal),
-        Font(R.font.misans_medium, FontWeight.Medium),
-        Font(R.font.misans_semibold, FontWeight.SemiBold),
         Font(R.font.misans_bold, FontWeight.Bold),
         Font(R.font.misans_heavy, FontWeight.ExtraBold)
     )
@@ -26,6 +21,6 @@ object CustomFonts {
 
 enum class FontType(val displayName: String, val fontFamily: FontFamily, val availableWeights: List<Int>) {
     SYSTEM("系统默认", FontFamily.Default, listOf(400)),
-    MI_SANS("Mi Sans", CustomFonts.MiSans, listOf(100, 200, 300, 400, 500, 600, 700, 800)),
+    MI_SANS("Mi Sans", CustomFonts.MiSans, listOf(400, 700, 800)),
     LXGW_WENKAI("霞鹜文楷", CustomFonts.LXGWWenKai, listOf(300, 400, 500))
 }
